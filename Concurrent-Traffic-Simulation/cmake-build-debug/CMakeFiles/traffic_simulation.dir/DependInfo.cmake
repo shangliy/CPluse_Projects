8b641e34b06b15349dbd72e06714b0d1960be663
shanglin.yang <shanglin.yang@jd.com>
2020-04-19 16:49:53 -0700

    update concurrent traffic simulation

---
+++ Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/Graphics.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/Intersection.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/Street.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/TrafficLight.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/TrafficSimulator-Final.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/src/Vehicle.cpp" "/home/shangliy/Projects/Codes/Concurrent-Traffic-Simulation/cmake-build-debug/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
